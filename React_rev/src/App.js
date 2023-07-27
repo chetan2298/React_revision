742989b3d55c2d05222fa7cf50c2748e6ca62cf7
chetan2298 <chaitanya.2298@gmail.com>
2023-07-26 20:13:08 -0700

    first Component

--- React_rev/src/App.js
+++ React_rev/src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ExpenseItem from "./Components/ExpenseItem";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Time is now.
-        </p>
-      </header>
+    <div>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
